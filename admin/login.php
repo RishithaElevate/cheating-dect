b0ee8c5d3f70201a3b78fc03dbe8d7474bcb36fd
RishithaElevate <rishitharajjoudula@gmail.com>
2025-10-22 21:13:06 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -7,7 +7,7 @@
        $myusername = mysqli_real_escape_string($db,$_POST['email']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM admin WHERE email = '$myusername' and password = '$mypassword'";
+      $sql = "SELECT id FROM admins WHERE email = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       // $active = $row['active'];
