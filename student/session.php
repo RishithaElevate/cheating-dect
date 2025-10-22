fbc10b8a05e7feeee60d594ec631855bb38aca0a
RishithaElevate <rishitharajjoudula@gmail.com>
2025-10-22 20:54:45 +0530

    Update session.php

--- student/session.php
+++ student/session.php
@@ -1,16 +1,17 @@
 <?php
-session_start();
-if (!isset($_SESSION['login_user'])) {
-    header("location:login.php");
-    exit;
-}
-
-include "../databaseconnect.php";
-
-$user_check = $_SESSION['login_user'];
-
-$ses_sql = mysqli_query($db, "SELECT email FROM students WHERE email = '$user_check'");
-$row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
-
-$login_session = $row['email'];
+   include "../databaseconnect.php";
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select email from students where email = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['email'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+      die();
+   }
 ?>
